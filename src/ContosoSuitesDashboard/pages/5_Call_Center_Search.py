34267c3109a736ef3ef5fb79dfa4309293092b1e
Shane <github@shaneosborne.com>
2025-01-28 12:05:16 +0000

    Ex 4 completed

--- src/ContosoSuitesDashboard/pages/5_Call_Center_Search.py
+++ src/ContosoSuitesDashboard/pages/5_Call_Center_Search.py
@@ -27,24 +27,24 @@ def make_azure_openai_embedding_request(text):
     )
 
 
-def make_cosmos_db_vector_search_request(query_embedding, max_results=5,minimum_similarity_score=0.5):
+def make_cosmos_db_vector_search_request(query_embedding, max_results=5, minimum_similarity_score=0.5):
     """Create and return a new vector search request. Key assumptions:
     - Query embedding is a list of floats based on a search string.
     - Cosmos DB endpoint, client_id, and database name stored in Streamlit secrets."""
-
+    
     cosmos_client_id = st.secrets["cosmos"]["client_id"]
     cosmos_credentials = DefaultAzureCredential(managed_identity_client_id=cosmos_client_id)
-
+ 
     cosmos_endpoint = st.secrets["cosmos"]["endpoint"]
     cosmos_database_name = st.secrets["cosmos"]["database_name"]
     cosmos_container_name = "CallTranscripts"
-
+ 
     # Create a CosmosClient
-    client = CosmosClient(url=cosmos_endpoint, credential=cosmos_key)
+    client = CosmosClient(url=cosmos_endpoint, credential=cosmos_credentials)
     # Load the Cosmos database and container
     database = client.get_database_client(cosmos_database_name)
-    container = database.get_container_client(cosmos_credentials)
-
+    container = database.get_container_client(cosmos_container_name)
+ 
     results = container.query_items(
         query=f"""
             SELECT TOP {max_results}
@@ -58,14 +58,13 @@ def make_cosmos_db_vector_search_request(query_embedding, max_results=5,minimum_
                 VectorDistance(c.request_vector, @request_vector) > {minimum_similarity_score}
             ORDER BY
                 VectorDistance(c.request_vector, @request_vector)
-            """,
+        """,
         parameters=[
             {"name": "@request_vector", "value": query_embedding}
         ],
         enable_cross_partition_query=True
     )
-
-    # Create and return a new vector search request
+ 
     return results
 
 
